03e9aff305a257f1dab7d0eb31da26ee4d14744e
siyam-hossain <siyam.cybersoul@gmail.com>
2025-08-04 22:11:23 +0600

    siyam: run or not

--- src/main/java/com/sh/nexorabd/App.java
+++ src/main/java/com/sh/nexorabd/App.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("test.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
